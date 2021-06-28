7cd771ada56b8a4d8d8d628d140da50e77c13950
Rick <rick.santos@webjump.com.br>
2021-06-28 01:41:15 -0300

    Strings

---
+++ strings.php
@@ -0,0 +1,11 @@
+<?php
+
+$idade = 19;
+
+//echo 'Olá mundo! Minha idade é ' . $idade . ' anos.';
+
+//echo "Olá mundo! Minha idade é $idade anos.";
+
+//echo "Olá mundo!\n";
+echo "Olá mundo!" . PHP_EOL;
+echo "\tEu tenho $idade anos";
